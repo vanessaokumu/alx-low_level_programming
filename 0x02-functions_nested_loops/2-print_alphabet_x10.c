01fab3c7e62e3fd17155004946d13200e0c1e13f
vanessaokumu <okumuvanessa@gmail.com>
2022-09-14 07:18:48 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,13 @@ void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
- 
+
 	i = 0;
 
-	while ( i < 10)
+	while (i < 10);
 	{
 		ch = 'a';
-		whille (ch <= 'z')
+		while (ch <= 'z');
 		{
 			_putchar(ch);
 			ch++;
