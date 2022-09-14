4ef283b35751ba3d6ac5affd8a0dca31fa7ecdb8
vanessaokumu <okumuvanessa@gmail.com>
2022-09-14 10:12:29 -0700

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,24 +7,15 @@
  */
 int main(void)
 {
-	unsigned long int sum3, sum5, sum;
-	int i;
-
-	sum3 = 0;
-	sum5 = 0;
-	sum = 0;
+	int i, sum = 0;
 
 	for (i = 0; i < 1024; ++i)
 	{
-		if ((i % 3) == 0)
-		{
-			sum3 = sum3 + i;
-		} else if ((i % 5) == 0)
-		{
-			sum5 = sum5 + i;
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
 		}
-	}
-	sum = sum3 + sum5;
-	printf("%1u\n", sum);
+
+	printf("%d\n", sum);
+
 	return (0);
 }
