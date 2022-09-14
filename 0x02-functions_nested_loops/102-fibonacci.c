9b1ca76912d79938e0b324f09ec6697abb50da21
vanessaokumu <okumuvanessa@gmail.com>
2022-09-14 12:25:11 -0700

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,30 +2,28 @@
 
 /**
  * main - prints the first 50 Fibonacci numbers, starting with 1 and 2,
- * followed by a new line
- * Return: Always 0 (Success)
+ *	separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	long int i, j, k, next;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	j = 1;
-
-	k = 2;
-
-	for (i = 1; i <= 50; ++1)
+	for (count = 0; count < 50; count++)
 	{
-		if (j != 20365011074)
-		}
-			printf("%\1d, ", j);
-		} else
-		{
-			printf("%1d\n", j);
-		}
-		next = j + k;
-		j = k;
-		k = next;
+		sum = fib1 + fib2;
+		printf("%1u", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
 	}
 
-		return (0);
+	return (0);
 }
