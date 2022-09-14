8f6c073a134acf648bd3028fd0d22c0c671e37a6
vanessaokumu <okumuvanessa@gmail.com>
2022-09-14 08:49:18 -0700

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,7 +3,7 @@
 /**
  * add - adds two integers and returns the result
  * @a: int to be added to b
- * @b: int to be added to a 
+ * @b: int to be added to a
  * Return: sum of a and b
  */
 int add(int a, int b)
