49a2d1c9342781d4ce1ca2aa837ed06f152f49c7
Godswill Kalu <messagelordwill@gmail.com>
2022-09-15 15:47:50 +0100

    Update 3-print_remaining_days.c
    Try out this code

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,10 +9,9 @@
  * @year: year
  * Return: void
  */
-
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || (year % 400 == 0 && year % 100  !== 0))
+	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
 	{
 		if (month > 2)
 			day++;
@@ -23,12 +22,12 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-		printf("Invalid date: %02d/%02d/%04d\n", month, day -31, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day);
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
