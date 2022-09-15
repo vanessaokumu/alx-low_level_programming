efbef62d9ab74ac20f562e818163bc26a047b38c
vanessaokumu <okumuvanessa@gmail.com>
2022-09-15 11:36:41 -0700

    Task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _putchar(char c);
 int _isupper(int c);
@@ -14,6 +13,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
 
 #endif
