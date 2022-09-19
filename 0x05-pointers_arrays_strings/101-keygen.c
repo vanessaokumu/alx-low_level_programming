fe56a2bfe461c531ae26136b76513a11010a0cd2
vanessaokumu <okumuvanessa@gmail.com>
2022-09-19 07:42:53 -0700

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,6 +11,7 @@
 int main(void)
 {
 	int pass[100];
+	int i, sum, n;
 
 	sum = 0;
 
@@ -20,7 +21,7 @@ int main(void)
 	{
 		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
-		putchar(pass[i} + '0');
+		putchar(pass[i] + '0');
 		if ((2772 - sum) - '0' < 78)
 		{
 			n = 2772 - sum -'0';
