a05779a4e77687cd39111c26d579a92737d3c959
vanessaokumu <okumuvanessa@gmail.com>
2022-09-26 13:32:34 -0700

    4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,19 +13,16 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	int j, i = 0;
 
-	for (i = 0; *s != '\0'; i++)
+	while (s[i])
 	{
-		for (j =0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j]; j++)
 		{
-			if (*s == accept[j])
-			{
-				return (s);
-			}
+			if (s[i] == accept[j])
+				return (s + i);
 		}
-		s++;
+		i++;
 	}
-	return (NULL);
+	return (0);
 }
-
