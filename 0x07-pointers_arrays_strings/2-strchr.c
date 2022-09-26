85d3370576afa23f395e7b647179df59c3e38320
vanessaokumu <okumuvanessa@gmail.com>
2022-09-26 10:39:06 -0700

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,6 +9,7 @@
  * @s: in the string
  *
  * Return: a pointer to the first occurrence of the character
+ * c in the string s, or NULL if the character is not found
  */
 
 char *_strchr(char *s, char c)
