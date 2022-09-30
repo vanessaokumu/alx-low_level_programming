5e68d572d3a7646c414dd4e941b16c7f6b0c42ea
vanessaokumu <okumuvanessa@gmail.com>
2022-09-30 08:34:22 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,16 +12,15 @@ int main(int argc, char **argv)
 {
 	int a, b;
 
-	if
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-		printf("%d\n", a * b);
-	return (0);
-
 	if (argc < 3)
 	{
 		printf("Error\n");
-	
 		return (1);
 	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	printf("%d\n", a * b);
+
+	return (0);
 }
