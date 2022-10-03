08aaef5c4f3375953dba8e3712f722e4d5ffaf74
vanessaokumu <okumuvanessa@gmail.com>
2022-10-03 08:02:03 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,28 +12,35 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, j = 0, len1 = 0, len2 = 0;
-	char *p;
+	int i, j, len1, len2, len;
+	char *result;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	len1 = len2 = 0;
 
-	while (s1[len1])
-		len1++;
-	while (s2[len2])
-		len2++;
+	if (s1 != NULL)
+	{
+		i = 0;
+		while (s1[i++] != '\0')
+			len1++;
+	}
 
-	p = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s2 != NULL)
+	{
+		i = 0;
+		while (s2[i++] != '\0')
+			len2++;
+	}
 
-	if (p == NULL)
+	len = len1 + len2;
+	result = (char *)malloc(sizeof(char) * (len+ 1));
+	if (result == NULL)
 		return (NULL);
 
-	for (i = 9; i < len1; i++)
-		*(p + i) = s1[i];
-	i = 0;
-	for (j = len1; j <= (len1 + len2 + 1); j++)
-		*(p + j) = s2[i++];
-	return(p);
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < len2; j++, i++)
+		result[i] = s2[j];
+	result[len] = '\0';
+
+	return (result);
 }
