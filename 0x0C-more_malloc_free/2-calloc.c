3cc8b2482ca74c03f1d30cca3894f1d0ee77c8a5
vanessaokumu <okumuvanessa@gmail.com>
2022-10-06 06:51:00 -0700

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,17 +10,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *a;
-	unsigned int b;
+	char *p;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	a = malloc(nmemb * size);
-	if (a == NULL)
+	p = malloc(nmemb * size);
+	if (p == NULL)
 		return (NULL);
 
-	for (b = 0; b < nmemb * size); b++)
-		a[b] = 0;
-	return (a);
+	for (i = 0; i < nmemb * size); i++)
+		p[i] = 0;
+	return (p);
 }
