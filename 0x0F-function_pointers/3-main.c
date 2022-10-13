f6e7751582440f2da8e771a8a9011f513b6a7548
vanessaokumu <okumuvanessa@gmail.com>
2022-10-13 06:15:32 -0700

    function pointers tasks

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,4 @@
-#include "3.calc.h"
+#include "3-calc.h"
 
 /**
  * main - performs simple operations
@@ -7,11 +7,10 @@
  *
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv[])
 {
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
+	int a = 0, b = 0, res = 0;
+	char s;
 
 	if (argc != 4)
 	{
@@ -19,28 +18,22 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
-
-	func = get_op_func(argv[2]);
-
-	if (!func)
+	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
-		exit(98);
+		exit(99);
 	}
 
-	o = *argv[2]];
-
-	if ((o == '/' || o == '%') && arg2 == 0)
+	s = argv[2][0];
+	if (s != '+' && s != '-' && s != '/' && s != '*' && s != '%')
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
 
-	result = func(arg1, arg2);
-
-	printf("%d\n", result);
-
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	res = (get_op_func(argv[2]))(a, b);
+	printf("%d\n", res);
 	return (0);
 }
