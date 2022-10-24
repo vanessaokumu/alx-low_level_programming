ba483527fd370e8c68bbd99acaaeaf3852734a79
vanessaokumu <okumuvanessa@gmail.com>
2022-10-24 08:42:50 -0700

    Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * list_len - cheecks the number of elements ina list_t list
+ * @h: name of the list
+ * Return: size of singly linked list
+ */
+size_t list_len(const list_t *h)
+{
+	int i;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		for (i = 1; h->next != NULL; j++)
+		{
+			h = h->next;
+		}
+		return (i);
+	}
+}
