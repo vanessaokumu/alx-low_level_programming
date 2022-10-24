0a135ae7e45846b23c905faeb58142fa086af6d0
vanessaokumu <okumuvanessa@gmail.com>
2022-10-24 11:27:38 -0700

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the first node of the list
+ */
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
+}
