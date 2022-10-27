d013b40fdbfc0f1b84a623d0453a76a39eeade97
vanessaokumu <okumuvanessa@gmail.com>
2022-10-27 02:09:38 -0700

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: head pointer
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}
