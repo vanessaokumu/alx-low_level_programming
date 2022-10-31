4b9a4d47e6b8667548fc4a2451a959a5a04e9b56
vanessaokumu <okumuvanessa@gmail.com>
2022-10-31 08:22:59 -0700

    task 6

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,11 +2,11 @@
 
 /**
  * get_endianness - checks the endianness
- * 
  * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
 	unsigned long int n = 1;
+
 	return (*(char *)&n);
 }
