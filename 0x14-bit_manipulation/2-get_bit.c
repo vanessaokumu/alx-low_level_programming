b1f7f516087c741bbb4f867ca162cae07f7baa57
vanessaokumu <okumuvanessa@gmail.com>
2022-10-31 07:54:08 -0700

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: int to print
+ * @index: the index, starting from 0 of the bit you want to get
+ *
+ * Return: the value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	return (n >> index & 1);
+}
