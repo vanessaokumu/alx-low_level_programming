6a88b987bbe14625662c4bff47c93a4b5ce00844
vanessaokumu <okumuvanessa@gmail.com>
2022-12-08 08:09:27 -0800

    returns the sum of all the data (n) of a dlistint_t linked list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,13 +1,13 @@
-#include "lists.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
 
 /**
- * sum_dlistint - returns the sum of all the data (n) of
- * a dlistint_t linked list
- * @head: pointer to the beginning of the linked list
  *
- * Return: sum of all data, 0 if the list is empty
+ * sum_dlistint - that returns the sum of all the data (n) of a list
+ * @head: variable pointer
+ *
+ * Return: the sum of all the data (n), 0 if list is empty
  */
 int sum_dlistint(dlistint_t *head)
 {
